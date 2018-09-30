862a6f974b62c6cc53086f82475c536be0edad9d
Tomek Owczarek <violeticus@gmail.com>
2018-09-30 11:43:57 +0200

    First solution

--- 01_javac/world/pl/lodz/srodmiescie/HelloWorld.java
+++ 01_javac/world/pl/lodz/srodmiescie/HelloWorld.java
@@ -1,5 +1,11 @@
+package world.pl.lodz.srodmiescie;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+    }
+    
+    class HelloPoland {
+
     }
 }
